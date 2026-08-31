2649d20457823a220934683d8c4d284bd779348c
Alessandro Tulioli <desmecad@hotmail.com>
2026-08-31 16:51:37 -0300

    chore: create solution structure following Clean Architecture

---
+++ src/AgroControl.Api/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapHealthChecks("/health");
+app.MapGet("/", () => Results.Ok(new
+{
+    service = "AgroControl.Api",
+    status = "running",
+    version = "v1"
+}));
+
+app.Run();
+
+public partial class Program;

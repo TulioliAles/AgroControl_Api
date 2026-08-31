0628314bfeeddfe4c03ff6abb0c3d94eb3557617
Alessandro Tulioli <desmecad@hotmail.com>
2026-08-31 16:43:49 -0300

    feat(domain): define measurement units

---
+++ src/AgroControl.Domain/Inputs/MeasurementUnit.cs
@@ -0,0 +1,15 @@
+namespace AgroControl.Domain.Inputs;
+
+public enum MeasurementUnit
+{
+    Unit = 1,
+    Kilogram = 2,
+    Gram = 3,
+    Liter = 4,
+    Milliliter = 5,
+    Ton = 6,
+    Bag = 7,
+    Box = 8,
+    Package = 9,
+    Dose = 10
+}

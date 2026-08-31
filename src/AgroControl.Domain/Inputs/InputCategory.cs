9ba6c324946644113540739e64123ca09dfd9f4f
Alessandro Tulioli <desmecad@hotmail.com>
2026-08-31 16:43:44 -0300

    feat(domain): define agricultural input categories

---
+++ src/AgroControl.Domain/Inputs/InputCategory.cs
@@ -0,0 +1,17 @@
+namespace AgroControl.Domain.Inputs;
+
+public enum InputCategory
+{
+    Seed = 1,
+    Seedling = 2,
+    Fertilizer = 3,
+    SoilCorrective = 4,
+    Inoculant = 5,
+    Biofertilizer = 6,
+    Pesticide = 7,
+    BiologicalControl = 8,
+    Adjuvant = 9,
+    AnimalNutrition = 10,
+    VeterinaryProduct = 11,
+    Other = 99
+}
